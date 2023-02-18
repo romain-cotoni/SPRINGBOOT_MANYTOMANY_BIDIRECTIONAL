f6eb53382c02208b5b145742f046224300785054
romain-cotoni <romain.cotoni@gmail.com>
2023-02-18 12:59:24 +0100

    First commit

---
+++ src/main/java/fr/springboot/manytomany/model/Table2Model.java
@@ -0,0 +1,65 @@
+package fr.springboot.manytomany.model;
+
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "table2")
+public class Table2Model {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_tbl2")
+	private int id;
+	
+	@Column(name="label_tbl2", length=50)
+	private String label;
+
+	@Column(name="level_tbl2")
+	private Integer level;
+	
+	@JsonIgnore
+	@ManyToMany(mappedBy = "table2Models")
+	private Set<Table1Model> table1Models;
+	
+	public Table2Model() {
+	}
+
+	public int getId() {
+		return id;
+	}
+	
+	public String getLabel() {
+		return label;
+	}
+
+	public void setLabel(String label) {
+		this.label = label;
+	}
+
+	public Integer getLevel() {
+		return this.level;
+	}
+
+	public void setLevel(Integer level) {
+		this.level = level;
+	}
+
+	public Set<Table1Model> getTable1Models() {
+		return table1Models;
+	}
+
+	public void setTable1Models(Set<Table1Model> table1Models) {
+		this.table1Models = table1Models;
+	}
+	
+}
